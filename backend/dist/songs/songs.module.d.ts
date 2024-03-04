b7e021c669161127cf6bcabc8c30f138c88e1e9d
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-03-04 22:02:36 +0200

    Delete unnecessary files and added songs module

---
+++ backend/dist/songs/songs.module.d.ts
@@ -0,0 +1,6 @@
+import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
+export declare class SongsModule {
+    controllers: [SongsController];
+    providers: [SongsService];
+}
