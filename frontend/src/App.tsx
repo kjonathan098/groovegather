8d4f2808a85c7728352f47aca7c23ae59dc5f717
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-03-05 15:01:56 +0200

    Created Grid for aside and main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, Center, ChakraProvider, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
-	return <ChakraProvider>Hello</ChakraProvider>
+	return (
+		<ChakraProvider>
+			<Center bg={'green'}>
+				<Box maxW={'1600px'} minW={'100%'} h={'100%'} bg={'red'}>
+					<Grid templateAreas={{ base: `"main"`, lg: ` "aside main"` }} templateColumns={{ base: '1fr', lg: '200px 1fr' }} h={'100%'}>
+						<Show above="lg">
+							<GridItem area="aside" h={'100vh'} overflow={'scroll'} bg={'yellow'}>
+								Side Bar
+							</GridItem>
+						</Show>
+
+						<GridItem area="main" bg={'green'}>
+							Main
+						</GridItem>
+					</Grid>
+				</Box>
+			</Center>
+		</ChakraProvider>
+	)
 }
 
 export default App
