67ec63ac3a504c015e2dc782d4a2c226ae12f053
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-03-06 16:30:53 +0200

    style: added banner and title

--- frontend/src/components/SongDisplay/Hero.tsx
+++ frontend/src/components/SongDisplay/Hero.tsx
@@ -1,8 +1,22 @@
 import { Box, Center, Image } from '@chakra-ui/react'
 import heroBanner from '../../media/banner_image.jpeg'
+import grooveGatherTittle from '../../media/groovegather_title.png'
 
 const Hero = () => {
-	return <Image src={heroBanner} maxH={{ base: '100%' }} w={'100%'} objectFit={'cover'} />
+	return (
+		<Box
+			height={'100%'} // Example height, adjust as needed
+			width="100%"
+			backgroundPosition="center"
+			backgroundRepeat="no-repeat"
+			backgroundSize="cover"
+			backgroundImage={`url(${heroBanner})`}
+		>
+			<Center h={'100%'}>
+				<Image src={grooveGatherTittle} objectFit={'cover'} w={'50%'} />
+			</Center>
+		</Box>
+	)
 }
 
 export default Hero
